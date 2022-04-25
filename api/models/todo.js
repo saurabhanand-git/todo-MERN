029d0e6f44f3a294a1ca91269338387db736dd23
saurabhanand-git <saurabhk3001@gmail.com>
2022-04-25 21:22:05 +0530

    1st commit

---
+++ api/models/todo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+    text:{
+        type: String,
+        required:true
+    },
+    complete:{
+        type: Boolean,
+       default:false
+    },
+    timestamp:{
+        type: String,
+        default:Date.now()
+    }
+})
+
+const Todo = mongoose.model("Todo", todoSchema);
+module.exports = Todo;  
